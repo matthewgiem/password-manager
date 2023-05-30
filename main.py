fe9b6264a10ac7bf68bdb31b28b39e5aead4ef98
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-05-30 15:13:22 +0700

    craete basic layout

--- main.py
+++ main.py
@@ -12,6 +12,30 @@ window.config(pady=20, padx=20)
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+email_username = Label(text="Email/Username:")
+email_username.grid(column=0, row=2)
+
+email_username_entry = Entry(width=35)
+email_username_entry.grid(column=1, row=2, columnspan=2)
+
+password = Label(text="Password:")
+password.grid(column=0, row=3)
+
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3)
+
+password_button = Button(text="Generate Password")
+password_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
